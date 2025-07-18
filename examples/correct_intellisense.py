16c545a729f0e2f15eb646efcb359010ff531fa3
Jerome Etienne <jerome.etienne@gmail.com>
2025-07-18 15:57:14 +0200

    correct intellisense in vscode

---
+++ examples/correct_intellisense.py
@@ -0,0 +1,24 @@
+# Experiment to handle intellisense in VSCode
+from gsp import core, transform
+import matplotlib.pyplot as plt
+
+import gsp
+gsp.use("matplotlib")
+
+
+canvas = core.Canvas(512, 512, 100.0)
+
+# Fixed viewport sizes and positions (in pixels)
+# core.Viewport(canvas, 0, 0, 256, 256, (1,0,0,1))
+# core.Viewport(canvas, 0, 256, 256, 256, (0,1,0,1))
+# core.Viewport(canvas, 256, 0, 256, 256, (0,0,1,1))
+# core.Viewport(canvas, 256, 256, 256, 256, (1,1,0,1))
+
+# Proportional viewport sizes and positions
+core.Viewport(canvas, 0.0, 0.0, 0.5, 0.5, (1,0,0,1))
+core.Viewport(canvas, 0.0, 0.5, 0.5, 0.5, (0,1,0,1))
+core.Viewport(canvas, 0.5, 0.0, 0.5, 0.5, (0,0,1,1))
+core.Viewport(canvas, 0.5, 0.5, 0.5, 0.5, (1,1,0,1))
+
+canvas.render("output/viewport-multiple.png")
+plt.show()
