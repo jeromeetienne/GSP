4a7e15ec76f6e282edaef3a33b6f1676bbd85e16
Jerome Etienne <jerome.etienne@gmail.com>
2025-08-03 18:13:45 +0200

    more

--- examples/dimension_reduction/libs/dim_reduc_display.py
+++ examples/dimension_reduction/libs/dim_reduc_display.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import gsp as gsp
+from gsp.matplotlib import core, visual, glm
 
 def display_matplotlib_scatter_plot(points_fitted, points_colors):
     """
@@ -28,17 +29,38 @@ def display_matplotlib_point_labels(points_fitted, labels_original):
         label_center = np.mean(label_points, axis=0)
         plt.text(label_center[0], label_center[1], str(label), fontsize=12, ha='center', va='center')
 
-def display_gsp(points_fitted, points_colors, canvas_width=1024, canvas_height=1024):
+def display_gsp_pixels(points_fitted, points_colors, canvas_width=1024, canvas_height=1024):
 
     # Create a canvas and viewport
     canvas = gsp.core.Canvas(canvas_width, canvas_height, 100.0)
     viewport = gsp.core.Viewport(canvas, 0, 0, canvas_width, canvas_height, [1,1,1,1])
 
     # Create a Pixels visual
-    # pixels = visual.Pixels(points_fitted, colors=gsp.black)
-    pixels = gsp.visual.Pixels(points_fitted, colors=points_colors)
+    # pixels = visual.Pixels(points_fitted_3d, colors=gsp.black)
+    # pixels = gsp.visual.Pixels(points_fitted_3d, colors=points_colors)
 
-    pixels.render()
 
-    viewport.render()
+    # Create a Pixels visual
+    pixels = visual.Pixels(points_fitted, colors=gsp.black)
+    # pixels = visual.Pixels(points_fitted, colors=points_colors)
+
+    # display in points
+    # sizes = glm.float(len(point_positions))
+    # sizes[...] = 40
+    # pixels = visual.Points(point_positions, sizes, gsp.grey, gsp.black, [0.5])
+    # pixels = visual.Points(point_positions, sizes, point_colors, gsp.black, [0])
+
+    from .camera import Camera
+    camera = Camera("ortho")
+    camera.connect(viewport, "motion",  pixels.render)
+    camera.run()
+    # # Connect the camera to the viewport
+    # camera = Camera("perspective", theta=-30, phi=0, log_fps_enabled=True, scale=5.0)
+
+    # camera.connect(viewport, "motion", pixels.render)
+
+    # # Render the pixels visual
+    # print('Rendering pixels visual...')
+    # camera.run()
+
 
