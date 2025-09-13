d18d9b0aa57b4ec0dd9605eb0fc95b64c76d34f7
Jerome Etienne <jerome.etienne@gmail.com>
2025-09-13 14:07:06 +0200

    minor cleanup

--- gsp_sc/TODO.md
+++ gsp_sc/TODO.md
@@ -2,9 +2,9 @@
 - implement 3d
   - https://github.com/rougier/matplotlib-3d/ <- YES
   - https://github.com/rougier/tiny-renderer/blob/master/bunny.py
-- implement network client/server over http
+
+- DONE implement network client/server over http
   - `./network/client.py` send a scene.json
   - `./network/server.py` receive a scene.json and render it and send back a png
-
 - DONE multiple viewports
 - DONE from/to file
--- gsp_sc/examples/basic.py
+++ gsp_sc/examples/basic.py
@@ -1,15 +1,12 @@
 import gsp_sc.src as gsp_sc
 import numpy as np
-import matplotlib.image as mpl_img
-
-import mpl3d.camera
-import mpl3d.glm
-import mpl3d.mesh
 
 import os
 __dirname__ = os.path.dirname(os.path.abspath(__file__))
 
-
+###############################################################################
+# Create a GSP scene
+#
 canvas = gsp_sc.core.Canvas(width=512, height=512, dpi=100)
 viewport = gsp_sc.core.Viewport(
     origin_x=0,
@@ -20,7 +17,9 @@ viewport = gsp_sc.core.Viewport(
 )
 canvas.add(viewport=viewport)
 
+###############################################################################
 # Add some random points
+#
 n_points = 100
 positions_np = np.random.uniform(-0.5, 0.5, (n_points, 2)).astype(np.float32)
 sizes_np = np.random.uniform(5, 10, n_points).astype(np.float32)
@@ -29,6 +28,8 @@ pixels = gsp_sc.visuals.Pixels(
 )
 viewport.add(pixels)
 
-# Render the scene
+###############################################################################
+# Render the scene with matplotlib
+#
 renderer = gsp_sc.renderer.matplotlib.MatplotlibRenderer()
 renderer.render(canvas)
